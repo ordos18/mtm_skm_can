127fc305860feab2fd0086b14541d55fe10a98c2
Maciek <ordos18@gmail.com>
2020-01-13 19:26:15 +0100

    CAN advanced zad 2 v1.0

--- advanced/main.c
+++ advanced/main.c
@@ -1,25 +1,32 @@
-// Doprowadzic do dzialania ponizszy program
-// Uzyc Acceptance Filter
-// Nie uzywac przerwan
+// Doprowadzic do dzialania ponizsze programy
+// Ostateczny test przeprowadzic laczac 3 zestawy uruchomieniowe
+// Podpowiedz: ID odbiornika okresla stan przycisku w trakcie resetu
 
 #include "led.h"
 #include "keyboard.h"
 #include "can.h"
 
-#define ID 17
+#define EVM_A_ID 17
+#define EVM_B_ID 18
 
 int main(){
-	unsigned char ucMsgCtr=0;
+	unsigned int 	uiID;
+	unsigned char 	ucMsgCtr=0;
+	
 	Led_Init();
 	Keyboard_Init();
 	Can1_InitAsTx();
-	Can2_InitAsRx(ID);
+	
+	if (eKeyboard_Read()==BUTTON_1) uiID=EVM_B_ID; else uiID=EVM_A_ID;
+	Can2_InitAsRx(uiID);
 	
 	while(1){ 
 		unsigned int 	uiID;
-		if (eKeyboard_Read()==BUTTON_1) uiID=ID; else uiID=0;
 		
-		if (ucCan1_TxFree())  Can1_SendByte(uiID,ucMsgCtr++);
-		if (ucCan2_RxReady()) Led_ShowByte(ucCan2_ReceiveByte());
+		if (eKeyboard_Read()==BUTTON_1) uiID=EVM_B_ID; else uiID=EVM_A_ID;
+		if (ucCan1_TxFree()) Can1_SendByte(uiID,ucMsgCtr++);
+		
+		if (ucCan2_RxReady()) Led_ShowByte(ucCan2_ReceiveByte());		
 	}
 }
+
